f1ae29a0c8b58cb18953f4fc379f2ddfd43d028a
andrewquince <andrewquinceee@gmail.com>
2026-09-15 10:25:38 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let filteredUsers = users.filter((user) => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  let totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  return totalAge / filteredUsers.length;
 }
